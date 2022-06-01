d3498716a20d407f298f7478f22dbc2be495c3bf
RedFlame <76979559+RedFlamer@users.noreply.github.com>
2022-06-01 15:41:49 +0100

    Delete worldholder.lua

--- Map-Based Optimizations/worldholder.lua
+++
@@ -1,7 +0,0 @@
-Hooks:PostHook(WorldHolder, "create_world", "map_optimizations_create_world", function(self)
-	if MBO.settings.remove_occluder_limit then
-		World:occlusion_manager():set_max_occluder_tests(1000) -- epic
-	else
-		World:occlusion_manager():set_max_occluder_tests(5) -- epicn't
-	end
-end)
